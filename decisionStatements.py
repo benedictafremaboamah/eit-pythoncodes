7472f2f14ed577b0cd6df3bef2670bbfc01a2ffd
Benedicta Frema Boamah <bfboamah9@gmail.com|>
2022-03-09 11:09:00 +0000

    while statements

--- decisionStatements.py
+++ decisionStatements.py
@@ -1,8 +1,40 @@
+from operator import length_hint
+from xml.dom.pulldom import START_ELEMENT
+
+
 age=19
-#if statement
+# if statement
 if age< 18:
     print("Still growing")
 elif age>= 18 and age < 21:
     print("You are an adolescent")
 else:
     print("You are an adult")
+
+#for in statement
+users=["Frema", "Akosua", "Kwasi", "Abdul"]
+for user in users:
+    print(user)
+
+# while statement
+num=10
+stepper=0
+while (stepper<10):
+    print (stepper)
+    stepper +=1
+
+# while statement printing even numbers
+num=10
+stepper=0
+while (stepper<num):
+    if (stepper % 2)== 0:
+        print (stepper)
+    stepper +=1
+    
+# printing length   
+users=["Frema", "Akosua", "Kwasi", "Abdul"]
+index=0
+length= len(users)
+while index<length:
+    print(users[index])
+    index +=1
