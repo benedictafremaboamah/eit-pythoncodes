97c0baf523cfa41234ea659555ee5bb5e82abb68
Benedicta Frema Boamah <bfboamah9@gmail.com|>
2022-03-10 15:48:43 +0000

    dictionary

--- dataStructures.py
+++ dataStructures.py
@@ -1,2 +1,18 @@
-my_fruits=["apple", "orange","strawberry"]
-print(my_fruits)
+my_list=["apple", "orange","strawberry"]
+print(my_list)
+
+my_list.append("Vero")
+print(my_list[1])
+
+my_list[0]="Blackberry"
+print(my_list)
+
+my_list.insert(-1, "Pineapple")
+print(my_list)
+
+my_list.remove("Pineapple")
+print
+print(my_list)
+
+my_dict={"id":24, "name":"Frema", "is programmer":True, "salary":1.2,"loans":[{"package":"Student loan", "amount":3500}, {"package":"Car loan", "amount":425}] ,"languages":[{"lang":"French", "proficiency":100}, {"lang":"Spanish", "proficiency":60}]}
+print(my_dict.get("loans")[0].get("amount"))
